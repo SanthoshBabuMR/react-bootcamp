48cafea3c4f84e2738a9c0535fb130bce5ce08b1
Santhosh Babu M R <santhosh.infotec@gmail.com>
2019-06-07 11:16:29 +0530

    updated

---
+++ portfolio/src/data/projects.js
@@ -0,0 +1,25 @@
+import project1 from '../assets/project1.png';
+import project2 from '../assets/project2.png';
+import project3 from '../assets/project3.png';
+
+const PROJECTS = [{
+    id: 1,
+    title: 'Example React Application',
+    description: 'A React App that I built, involving JS and core web dev concepts!',
+    link: 'https://github.com/SanthoshBabuMR/learning-requireJS',
+    image: project1
+}, {
+    id: 2,
+    title: 'My Presentations',
+    description: 'A Web Dev presentation that I built for Knowledge transfer among the team members',
+    link: 'https://github.com/SanthoshBabuMR/sb-presentation',
+    image: project2
+}, {
+    id: 3,
+    title: 'Core UI Snippets',
+    description: 'Collection of core ui snipppets, that I developed during my career days',
+    link: 'https://github.com/SanthoshBabuMR/core-uiSnippets',
+    image: project3
+}];
+
+export default PROJECTS;
